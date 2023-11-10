cbf6f56bb4ae21fdd8e4275b15258ede2a1e36d7
yum14 <yumurata1214@gmail.com>
2023-11-10 14:41:47 +0900

    inertia-vue.js

---
+++ resources/js/Pages/Inertia/Index.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+import { ref } from 'vue';
+
+defineProps({
+    blogs: Array
+});
+
+</script>
+
+<template>
+    <div>
+        <div v-if="$page.props.flash.message" class="bg-blue-300">
+            {{ $page.props.flash.message }}
+        </div>
+        <ul>
+            <li v-for="blog in blogs" :key="blog.id">
+                件名: <Link class="text-blue-400" :href="route('inertia.show', { id: blog.id })">{{ blog.title }}</Link>
+                本文: {{ blog.content }}
+            </li>
+        </ul>
+        ああああああああ
+    </div>
+</template>
